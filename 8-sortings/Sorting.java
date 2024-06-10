9c78f3c0bc62db0183246a24f68e2c08b2ae025f
kuldeepsinh Jhala <kuldeephjhala@gmail.com>
2024-06-10 17:58:51 +0530

    sorting

--- 8-sortings/Sorting.java
+++ 8-sortings/Sorting.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Sorting {
 
     public static void bubbleSortBrutForce(int arr[]) {
@@ -45,6 +47,40 @@ public class Sorting {
         }
     }
 
+    public static void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            // finding out correct position to insert
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            // insertion
+            arr[prev + 1] = curr;
+        }
+    }
+
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        // sorting
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -52,10 +88,26 @@ public class Sorting {
         System.out.println();
     }
 
+    public static void printArrInteger(Integer arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 5, 4, 1, 3, 2 };
+        // int arr[] = { 5, 4, 1, 3, 11, 3, 44, 23, 43, 546, 67576, 8, 6, 1, 0 };
+        int arr[] = { 5, 4, 3, 2, 1 };
+        Integer arrInteger[] = { 1, 2, 3, 4, 5 };
         // bubbleSortOptimised(arr);
-        selectionSort(arr);
+        // selectionSort(arr);
+        // insertionSort(arr);
+        // Arrays.sort(arr);
+        // Arrays.sort(arr, 0, 3);
+        // Arrays.sort(arrInteger, Collections.reverseOrder());
+        // Arrays.sort(arrInteger, 0, 3, Collections.reverseOrder());
+        // printArrInteger(arrInteger);
+        countingSort(arr);
         printArr(arr);
     }
 }
